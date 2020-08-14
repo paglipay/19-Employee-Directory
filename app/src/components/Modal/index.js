613ab91846c821fc18ba18aa96c359e806391a0a
Paul Aglipay <paglipay@gmail.com>
2020-08-14 12:53:16 -0700

    React bootstrap modal fix

--- app/src/components/Modal/index.js
+++ app/src/components/Modal/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
